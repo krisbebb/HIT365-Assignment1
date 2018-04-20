a736a392fc4e78fe53901311a729880192065584
krisbebb <krisbebb@gmail.com>
2018-04-20 21:32:59 +1000

    switch case manager

--- payroll.c
+++ payroll.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+int prompt(void);
+
+int main(void){
+  int emp_type = prompt();
+  switch(emp_type) {
+    case 1:
+      printf("Manager");
+      break;
+      
+  }
+
+}
+
+int prompt(void) {
+  int paycode;
+  printf("Enter employee paycode (1,2,3,4,Q): ");
+  scanf("%d", &paycode);
+  return paycode;
+}
