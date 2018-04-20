578195f5b59b0624da0d4e625b9d9a4e8674fb4e
krisbebb <krisbebb@gmail.com>
2018-04-20 22:55:34 +1000

    paycode prompt error safe

--- payroll.c
+++ payroll.c
@@ -1,21 +1,58 @@
 #include <stdio.h>
 
-int prompt(void);
+char prompt(void);
+
 
 int main(void){
-  int emp_type = prompt();
-  switch(emp_type) {
-    case 1:
-      printf("Manager");
-      break;
-      
+  int total_emps;
+  int manager_emps;
+  int hourly_emps;
+  int commission_emps;
+  int piece_emps;
+
+  float total_wages;
+  float manager_wages;
+  float hourly_wages;
+  float commission_wages;
+  float piece_wages;
+
+  char emp_type;
+  int finish = 0;
+
+  while (!finish) {
+    emp_type = prompt();
+    switch(emp_type) {
+      case '1':
+        printf("Manager");
+        break;
+      case '2':
+        break;
+      case '3':
+        break;
+      case '4':
+        break;
+        case 'Z':
+          printf("You entered too many characters. Please only enter '1' for manager, '2' for hourly, '3' for commission, '4' for pieceworker, or 'Q' to quit and see totals.\n");
+          break;
+      case 'Q':
+        finish = 1;
+        break;
+      default:
+        printf("Unrecognised paycode '%c'. Please only enter '1' for manager, '2' for hourly, '3' for commission, '4' for pieceworker, or 'Q' to quit and see totals.\n", emp_type);
+        break;
+    }
   }
 
 }
 
-int prompt(void) {
-  int paycode;
+ char prompt(void) {
+  char paycode[100];
   printf("Enter employee paycode (1,2,3,4,Q): ");
-  scanf("%d", &paycode);
-  return paycode;
+  scanf(" %s", &paycode[0]);
+  if (paycode[1] != '\0'){
+    return 'Z';
+  }
+
+  getchar();
+  return paycode[0];
 }
